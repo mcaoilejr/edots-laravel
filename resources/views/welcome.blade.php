d4844199b376b078cb93fc8b2ef3b5d3b21cce2b
mcaoilejr <manuel.caoile@deped.gov.ph>
2024-01-09 15:34:16 +0800

    Updated Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                 <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
                     <div class="m-4 m-lg-5">
                         <h1 class="display-5 fw-bold">Welcome!</h1>
-                        <p class="fs-4">This is the official eDOTS portal of SDO Kidapawan City.</p>
+                        <p class="fs-4">This is the official eDOTS portal of SDO Kidapawan City. 2024 Updated!</p>
                         <a class="btn btn-primary btn-lg" href="{{route('login')}}">LOGIN</a>
                     </div>
                 </div>
